89a809c5e56072dfc24b52041bbd5834f0bb16e0
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-11-18 16:07:10 -0700

    converting react components before moving css files

--- startup-react/src/play/play.jsx
+++ startup-react/src/play/play.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 export function Play() {
   return (
-    <main className='container-fluid bg-purple text-center'>
-      <div>play displayed here</div>
+    <main className="bg-purple">
+      <h1>5 Strikes and You're Out!</h1>
+      <div className="word" id="wordDisplay">
+      <span className="big-text">_ _ _ _ _</span>
+      </div>
+
+      <div className="input-container">
+        <label for="guessInput">Guess a letter:</label>
+        <input type="text" id="guessInput" maxlength="1"></input>
+        <button type="makeGuess()" className="btn btn-primary">Guess</button>
+      </div>
+
+      <div className="tally-container">
+        <span className="strike">|</span>
+        <span className="strike">|</span>
+        <span className="strike">|</span>
+        <span className="strike">|</span>
+        <span className="strike-fifth">|</span>
+      </div>
+
+      <div className="message" id="message"></div>
+
+      <div className="scoreboard">
+        <h2>Global scoreboard</h2>
+        <p id="globalScore">Top Score:10</p>
+      </div>
     </main>
   );
 }
--- startup-react/src/scoreboard/scoreboard.jsx
+++ startup-react/src/scoreboard/scoreboard.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 
 export function Scoreboard() {
   return (
-    <main className='container-fluid bg-purple text-center'>
-      <div>scoreboard displayed here</div>
+  <main className="container-fluid bg-purple text-light text-center">
+    <h1>Global Scoreboard</h1>
+
+    <table className="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Bob Esponja</td>
+                <td>20</td>
+            </tr>
+            <tr>
+                <td>Patricio Estrella</td>
+                <td>13</td>
+            </tr>
+            <tr>
+                <td>El gary</td>
+                <td>15</td>
+            </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
