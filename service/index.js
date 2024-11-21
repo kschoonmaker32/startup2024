0b65485377ebeefe035f713ebf561129ab5e3595
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-11-21 10:57:21 -0700

    added router

--- service/index.js
+++ service/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const app = express();
 const uuid = require('uuid');
+const app = express();
+
+let users = {};
+let scores = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+//router for service 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
@@ -12,7 +21,3 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-app.use(express.json());
-
-let users = {};
-let scores = [];
