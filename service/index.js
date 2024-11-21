f62932246eb5e1f4ac20fd98ac92a7f67753cd51
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-11-21 10:57:44 -0700

    delete placeholder endpoint

--- service/index.js
+++ service/index.js
@@ -13,10 +13,6 @@ app.use(express.json());
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Simon service' });
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
